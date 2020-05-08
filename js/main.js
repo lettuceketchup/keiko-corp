d0f5a2eb6e5003371e8ab58e9d0466c698c593c7
lettuceketchup <lettuceketchup@users.noreply.github.com>
2020-05-08 18:47:49 +0530

    Removed nicescroll.js

--- js/main.js
+++ js/main.js
@@ -1,72 +1,13 @@
 $(document).ready(function(){
 
-	//Navigation menu scrollTo
-	$('header nav ul li a').click(function(event){
-		event.preventDefault();
-		var section = $(this).attr('href');
-		var section_pos = $(section).position();
-
-		if(section_pos){
-			$(window).scrollTo({top:section_pos.top, left:'0px'}, 1000);
-		}
-		
-	});
-
-	$('.app_link').click(function(e){
-		event.preventDefault();
-		$(window).scrollTo({top:$("#hero").position().top, left:'0px'}, 1000);		
-	});
-
-
-
-
-
-
-
-
 	//Show & Hide menu on mobile
 	$('.burger_icon').click(function(){
 		$('header nav').toggleClass('show');
 		$('header .burger_icon').toggleClass('active');
 	});
 
-	
-
-
-
-
-
-
-
-
-
 	//parallax effect initialization
 	$('.hero').parallax("50%", 0.3);
-
-
-
-
-
-
-
-
-	//Nice scroll initialization
-	$("html").niceScroll({
-		scrollspeed: 50,
-		autohidemode : false,
-		cursorwidth : 8,
-		cursorborderradius: 8,
-		cursorborder : "0",
-		background : "rgba(48, 48, 48, .4)",
-		cursorcolor : '#1f1f1f',
-		zindex : 999
-	});
-
-
-
-
-
-
 
 
 	//Testimonials slider initialization
@@ -81,11 +22,6 @@ $(document).ready(function(){
 		autoPlay : true,
 		transitionStyle : "fade"
 	});
-
-
-
-
-
 
 
 	//Mailchimp subscription form initialization
@@ -120,13 +56,6 @@ $(document).ready(function(){
 	function processing(hide, show){
 			$('#mc_submit i').removeClass(hide).addClass(show);
 	}
-
-
-
-
-
-
-
 
 	//Popup video
 	$('#play_video').click(function(e){
